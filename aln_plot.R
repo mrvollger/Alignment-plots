7856cc0a450bd4c08f062aa49b8e4cf510446ee2
mrvollger <mrvollger@gmail.com>
2020-06-21 21:53:02 -0700

    asd

--- aln_plot.R
+++ aln_plot.R
@@ -4,31 +4,20 @@ library(scales)
 library(RColorBrewer)
 library(dplyr)
 library(grid)
-#library(gridBase)
-library(gridExtra)
 library(data.table)
-library(gtable)
-#source("http://bioconductor.org/biocLite.R")
-#biocLite("karyoploteR")
-#BiocManager::install("karyoploteR")
-library(karyoploteR)
-library(GenomicRanges)
 library(cowplot)
-library(glue)
-library(ggplotify)
 library(patchwork)
-library(TxDb.Hsapiens.UCSC.hg38.knownGene)
-library(RColorBrewer)
 
 # check is rstudio and if so set the working direcotry to curdir 
 if(rstudioapi::isAvailable()){
   setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 }
 
-df= fread("nobackups/cmp.tbl"); df$cutid = as.factor(df$cutid)
+df= fread("nobackups/chm13_clint.paf.tbl"); df$cutid = as.factor(df$cutid)
 
+
+# make the identity histogram
 bins = min(250, nrow(df)/10)
-
 p1 = ggplot(data=df, aes(identity, fill=cutid, weight=aln_len))+ 
   geom_histogram(bins=bins) +
   ylab("bp") +
@@ -37,7 +26,7 @@ p1 = ggplot(data=df, aes(identity, fill=cutid, weight=aln_len))+
   theme_classic()  + theme(legend.position = "none") +
   scale_y_continuous(label=comma)
 
-
+# make the dot plot 
 p2 = ggplot(data=df, aes(x=x1, xend=x2, y=y1, yend=y2, fill=cutid, color=cutid)) + 
   geom_segment() +   coord_fixed(ratio = 1) +
   scale_fill_brewer(palette = "Spectral", direction=-1) +
@@ -47,4 +36,7 @@ p2 = ggplot(data=df, aes(x=x1, xend=x2, y=y1, yend=y2, fill=cutid, color=cutid))
   scale_y_continuous(label=comma) +
   scale_x_continuous(label=comma)
 
+# combine them 
 p3 = p1/p2 + plot_layout(heights = c(1, 3)); p3
+
+ggsave("~/Desktop/cen.png", plot=p3, height = 12, width = 8, dpi = 400)
