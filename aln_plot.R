dc1b6de440eac0dc71eb3755877808edf71f0821
mrvollger <mrvollger@gmail.com>
2020-06-22 14:45:43 -0700

    Fixed a tr dup issue

--- aln_plot.R
+++ aln_plot.R
@@ -13,7 +13,10 @@ if(rstudioapi::isAvailable()){
   setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 }
 
-df= fread("nobackups/chm13_clint.paf.tbl"); df$cutid = as.factor(df$cutid)
+H = 12
+W = 8
+
+df= fread("nobackups/glv9.paf.tbl"); df$cutid = as.factor(df$cutid)
 
 
 # make the identity histogram
@@ -26,8 +29,17 @@ p1 = ggplot(data=df, aes(identity, fill=cutid, weight=aln_len))+
   theme_classic()  + theme(legend.position = "none") +
   scale_y_continuous(label=comma)
 
+
+small = df
+nalns = 10e6
+if(nrow(small) > nalns){
+  small = small[sample(nrow(small), nalns),] # makes things too slow #, prob=small$matches/sum(small$matches)), ]
+}
+size = 0.5*(25.4 * W)/(nrow(small)); size
+
+
 # make the dot plot 
-p2 = ggplot(data=df, aes(x=x1, xend=x2, y=y1, yend=y2, fill=cutid, color=cutid)) + 
+p2 = ggplot(data=small, aes(x=x1, xend=x2, y=y1, yend=y2, fill=cutid, color=cutid)) + 
   geom_segment() +   coord_fixed(ratio = 1) +
   scale_fill_brewer(palette = "Spectral", direction=-1) +
   scale_color_brewer(palette = "Spectral", direction=-1) +
@@ -37,6 +49,10 @@ p2 = ggplot(data=df, aes(x=x1, xend=x2, y=y1, yend=y2, fill=cutid, color=cutid))
   scale_x_continuous(label=comma)
 
 # combine them 
-p3 = p1/p2 + plot_layout(heights = c(1, 3)); p3
+p3 = p1/p2 + plot_layout(heights = c(1, 3));
 
-ggsave("example.png", plot=p3, height = 12, width = 8, dpi = 400)
+
+density= 50
+ggsave("~/Desktop/example.png", plot=p3, height = density*H, width = density*W, units="mm", limitsize = F)
+
+
--- convert_paf.py
+++ convert_paf.py
@@ -44,53 +44,62 @@ def read_paf(f):
     for name in remove: del rtn[name] 
     return(rtn)
 
-def read_dict(paf_d, NCOLORS, minq):
-    df = pd.DataFrame(paf_d)
-    assert "de" == "de"
-    df["identity"] = 100 - 100*df["de"]
-    MINID = int(df.identity.quantile(minq))
-    df = df.loc[df.identity >= MINID]
-	
-    
-    df["cut"] = pd.qcut( df["identity"], NCOLORS, duplicates="drop")
-    df["cutid"] = pd.qcut( df["identity"], NCOLORS, duplicates="drop", labels=False)
-    NCOLORS = len(df.cutid.unique())
-    
-    # if the contig name is not unqiue, check for a split fasta, and then if exit if multiple queries 
-    if(len(df.q_name.unique())!=1):
-        split = df.q_name.str.extract('(.+):(\d+)-(\d+)', expand=True)
-        assert len(split[0].unique()) == 1, "There can be only one query contig"
-        offset = split[1].astype(int)
-        df.q_st = df.q_st + offset
-        df.q_en = df.q_en + offset
-        df.q_name = split[0]
-        df.q_len = df.q_len.sum()		
-	
-	
+def read_dict(paf_d, NCOLORS, minq, dup):
+	df = pd.DataFrame(paf_d)
+	# if the contig name is not unqiue, check for a split fasta, and then if exit if multiple queries 
+	if(len(df.q_name.unique())!=1):
+		split = df.q_name.str.extract('(.+):(\d+)-(\d+)', expand=True)
+		assert len(split[0].unique()) == 1, "There can be only one query contig"
+		offset = split[1].astype(int)
+		df.q_st = df.q_st + offset
+		df.q_en = df.q_en + offset
+		df.q_name = split[0]
+		df.q_len = df.q_len.sum()		
+
 	# set up the x and y names
-    rc = ( df.strand == "-")
-    df["x1"] = df["t_st"]
-    df["x2"] = df["t_en"]
-    df["y1"] = df["q_st"]
-    df["y2"] = df["q_en"]
-    df.loc[rc, "y1"] = df["q_en"][rc]
-    df.loc[rc, "y2"] = df["q_st"][rc]
-    
-    df = df[header + ["x1","x2","y1", "y2", "identity", "cutid"]]
-    return(df)
+	rc = ( df.strand == "-")
+	df["x1"] = df["t_st"]
+	df["x2"] = df["t_en"]
+	df["y1"] = df["q_st"]
+	df["y2"] = df["q_en"]
+	df.loc[rc, "y1"] = df["q_en"][rc]
+	df.loc[rc, "y2"] = df["q_st"][rc]
+	 
+	# set min identity 
+	df["identity"] = 100 - 100*df["de"]
+	MINID = int(df.identity.quantile(minq))
+	df = df.loc[df.identity >= MINID]
+
+	# de dup the huristic
+	if(not dup):
+		sys.stderr.write(f"\n{df.shape}\n")
+		df["score"] = df.aln_len * df.identity
+		df.sort_values(by="score", inplace=True)
+		dup = df.duplicated(subset=["x1", "y1"], keep="last") | df.duplicated(subset=["x2", "y2"], keep="last")
+		df=df[~dup]
+		sys.stderr.write(f"\n{df.shape}\n")
+
+	# set colors
+	df["cut"] = pd.qcut( df["identity"], NCOLORS, duplicates="drop")
+	df["cutid"] = pd.qcut( df["identity"], NCOLORS, duplicates="drop", labels=False)
+	NCOLORS = len(df.cutid.unique())
+
+	df = df[header + ["x1","x2","y1", "y2", "identity", "cutid"]]
+	return(df)
 
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument("infile", help="input paf with cigar string!")
 	parser.add_argument("outfile", help="output table with segments to draw in r")
+	parser.add_argument('--dup', help="Disable the dedup huristic for tandem repeats",  action="store_true", default=False)
 	parser.add_argument("-n", help="number of colors ", type=int, default=10)
-	parser.add_argument("-l", help="Remove the lowest quntile of alignemtns (default 0.5%)", type=float, default=0.005)
+	parser.add_argument("-l", help="Remove the lowest quntile of alignemtns", type=float, default=0.0)
 	parser.add_argument('-d', help="store args.d as true if -d",  action="store_true", default=False)
 	args = parser.parse_args()
 	
 	paf_d = read_paf(args.infile)
-	df = read_dict(paf_d, NCOLORS=10, minq=args.l)
+	df = read_dict(paf_d, NCOLORS=10, minq=args.l, dup=args.dup)
 	df.to_csv(args.outfile, index=False, sep="\t")
 
 
